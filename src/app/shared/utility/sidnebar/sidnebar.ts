e8b394fce2d17f235962eda817fcb2359aa5e8ec
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-06-09 23:47:41 -0500

    feat: agregar componente Sidnebar con su plantilla, estilos y pruebas

---
+++ src/app/shared/utility/sidnebar/sidnebar.ts
@@ -0,0 +1,31 @@
+import { Menu } from '../../../core/interfaces/login/menu';
+import { CommonModule, NgClass } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { MaterialModule } from '../../ui/material-module';
+
+@Component({
+  selector: 'app-sidnebar',
+  imports: [CommonModule, NgClass, RouterLink, MaterialModule],
+  templateUrl: './sidnebar.html',
+  styleUrl: './sidnebar.css'
+})
+export class Sidnebar {
+  @Input() isSidebarCollapsed = false;
+  @Input() userName = '';
+  @Input() tipoUsuario = '';
+  @Input() menuItems: Menu[] = [];
+  @Output() sidebarToggle = new EventEmitter<void>();
+
+  constructor(private readonly router: Router) {}
+
+  toggleSidebar() {
+    this.sidebarToggle.emit();
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    this.router.navigate(['/auth/login']);
+  }
+}
