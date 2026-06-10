19b8e5887d4b6403c70be3751fb898c1223b1c86
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-06-09 23:14:31 -0500

    feat: agregar servicios para gestión de contenido, cuestionarios, evaluaciones de riesgo y usuarios

---
+++ src/app/core/service/contenido.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Contenido } from '../interface/contenido';
+import { environment as ENV } from '../../../environments/environment';
+import { ApiResponse } from '../interface/apiResponse';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContenidoService {
+  private readonly http = inject(HttpClient);
+  
+  lista(){
+    return this.http.get<ApiResponse<Contenido[]>>(`${ENV.apiUrl}?ruta=contenidos`);
+  }
+
+  obtenerContenido(id: number){
+    return this.http.get<ApiResponse<Contenido>>(`${ENV.apiUrl}?ruta=contenidos&id=${id}`);
+  }
+
+  registrar(contenido: Contenido){
+    return this.http.post<ApiResponse<null>>(`${ENV.apiUrl}?ruta=contenidos`, contenido);
+  }
+
+  editar(contenido: Contenido){
+    return this.http.put(`${ENV.apiUrl}?ruta=contenidos`, contenido);
+  }
+
+  eliminar(id: number){
+    return this.http.delete<ApiResponse<null>>(`${ENV.apiUrl}?ruta=contenidos&id=${id}`);
+  }
+}
---
+++ src/app/core/service/opcionRespuesta.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment as ENV } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { OpcionRespuesta } from '../interface/opcionRespuesta';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OpcionRespuestaService {
+
+  private readonly http = inject(HttpClient);
+
+  crear(opcion: OpcionRespuesta): Observable<any> {
+    return this.http.post(`${ENV.apiUrl}?ruta=cuestionarios&accion=crear-opcion`, opcion);
+  }
+
+  actualizar(opcion: OpcionRespuesta): Observable<any> {
+    return this.http.put(`${ENV.apiUrl}?ruta=cuestionarios&accion=actualizar-opcion`, opcion);
+  }
+
+  eliminar(id: number): Observable<any> {
+    return this.http.delete(`${ENV.apiUrl}?ruta=cuestionarios&accion=eliminar-opcion&id=${id}`);
+  }
+}
