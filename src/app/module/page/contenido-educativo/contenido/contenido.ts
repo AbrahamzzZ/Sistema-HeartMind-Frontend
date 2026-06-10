c7509bc2ba9ee78eda667ae7140a9f76fee353fb
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-06-09 23:46:55 -0500

    feat: agregar componente Contenido con su plantilla y estilos

---
+++ src/app/module/page/contenido-educativo/contenido/contenido.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contenido',
+  standalone: false,
+  templateUrl: './contenido.html',
+  styleUrl: './contenido.scss',
+})
+export class Contenido {
+
+}
