4045d4d7e2366bbc29bb03f957659920cc8a23e7
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-06-09 23:46:28 -0500

    feat: agregar módulo CuestionariosAutoevaluacion con sus componentes y rutas

---
+++ src/app/module/page/cuestionarios-autoevaluacion/cuestionario/cuestionario.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cuestionario',
+  standalone: false,
+  templateUrl: './cuestionario.html',
+  styleUrl: './cuestionario.scss',
+})
+export class Cuestionario {
+
+}
