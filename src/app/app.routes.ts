b202f99644d9ef0c4b7a6be019e3206f38167ab0
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-06-10 23:32:22 -0500

    feat: agregar rutas para autenticación y navegación principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,76 @@
 import { Routes } from '@angular/router';
+import { AuthLayout } from './module/components/layout/auth-layout/auth-layout';
+import { Main } from './module/components/layout/main-layout/main/main';
 
 export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'auth/login',
+        pathMatch: 'full',
+    },
+    {
+        path: 'auth',
+        component: AuthLayout,
+        children: [
+            {
+                path: 'login',
+                loadChildren: () =>
+                    import('./module/pages/auth/auth-routing-module').then(
+                        (m) => m.AuthRoutingModule
+                    )
+            },
+            {
+                path: 'registro',
+                loadComponent: () =>
+                    import('./module/components/modal/registro-usuario/registro-usuario').then(
+                        m => m.RegistroUsuario
+                    )
+            },
+            {
+                path: '**',
+                redirectTo: 'login',
+                pathMatch: 'full'
+            }
+        ],
+    },
+    {
+        path: 'home',
+        component: Main,
+        children: [
+            {
+                path: 'contenido',
+                loadComponent: () =>
+                    import('./module/pages/contenido-educativo/contenido-educativo-module').then(
+                        (m) => m.ContenidoEducativoModule
+                    ),
+            },
+            {
+                path: 'evaluacion',
+                loadComponent: () =>
+                    import('./module/pages/evaluacion-riesgo-cardiovascular/evaluacion-riesgo-cardiovascular-module').then(
+                        (m) => m.EvaluacionRiesgoCardiovascularModule
+                    ), 
+            },
+            {
+                path: 'cuestionario',
+                loadComponent: () =>
+                    import('./module/pages/cuestionarios-autoevaluacion/cuestionarios-autoevaluacion-module').then(
+                        (m) => m.CuestionariosAutoevaluacionModule
+                    ), 
+            },
+            {
+                path: '**',
+                redirectTo: 'tray',
+                pathMatch: 'full'
+            }
+        ],
+    },
+    {
+        path: '**',
+        loadChildren: () =>
+        import('./module/pages/page-not-found/page-not-found-module').then(
+            (m) => m.PageNotFoundModule
+        ),
+    }
 
-    
 ];
