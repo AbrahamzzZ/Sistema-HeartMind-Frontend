9aa57d063f0720f78ff11a5be043a9a6b4f0d28b
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-06-13 23:37:50 -0500

    feat: cambios en los estilos de los modulos

--- src/app/module/pages/historial-medico/historial/historial.ts
+++ src/app/module/pages/historial-medico/historial/historial.ts
@@ -1,7 +1,9 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { AfterViewInit, Component, inject, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import * as XLSX from 'xlsx';
 import { EvaluacionRiesgoService } from '../../../../core/service/evaluacionRiesgo.service';
 import { HistorialEvaluacion } from '../../../../core/interface/evaluacion/historialEvaluacion';
-import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-historial',
@@ -9,10 +11,10 @@ import * as XLSX from 'xlsx';
   templateUrl: './historial.html',
   styleUrl: './historial.scss',
 })
-export class Historial implements OnInit {
+export class Historial implements OnInit, AfterViewInit {
 
-  private readonly evaluacionService = inject(EvaluacionRiesgoService);
-  historial: HistorialEvaluacion[] = [];
+  private readonly evaluacionService =
+    inject(EvaluacionRiesgoService);
 
   displayedColumns = [
     'fecha',
@@ -21,10 +23,17 @@ export class Historial implements OnInit {
     'riesgo'
   ];
 
+  dataSource = new MatTableDataSource<HistorialEvaluacion>([]);
+  totalRiesgoAlto = 0;
+  totalRiesgoModerado = 0;
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
   ngOnInit(): void {
-    const usuario = JSON.parse(
-      localStorage.getItem('user') || '{}'
-    );
+
+    const usuario =
+      JSON.parse(localStorage.getItem('user') || '{}');
 
     if (usuario.rol === 'Administrador') {
 
@@ -32,7 +41,7 @@ export class Historial implements OnInit {
         .obtenerHistoriales()
         .subscribe({
           next: (resp) => {
-            this.historial = resp.data;
+            this.cargarDatos(resp.data);
           }
         });
 
@@ -42,25 +51,69 @@ export class Historial implements OnInit {
         .obtenerHistorial(usuario.id)
         .subscribe({
           next: (resp) => {
-            this.historial = resp.data;
+            this.cargarDatos(resp.data);
           }
         });
     }
   }
 
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  private cargarDatos(
+    data: HistorialEvaluacion[]
+  ): void {
+
+    this.dataSource.data = data;
+
+    this.totalRiesgoAlto =
+      data.filter(
+        x => x.resultado_riesgo === 'Alto'
+      ).length;
+
+    this.totalRiesgoModerado =
+      data.filter(
+        x => x.resultado_riesgo === 'Moderado'
+      ).length;
+  }
+
+  aplicarFiltro(event: Event): void {
+
+    const filtro =
+      (event.target as HTMLInputElement)
+        .value
+        .trim()
+        .toLowerCase();
+
+    this.dataSource.filter = filtro;
+  }
+
   exportarExcel(): void {
 
-    const data = this.historial.map(item => ({
-      Fecha: item.fecha_evaluacion,
-      IMC: item.imc,
-      Puntaje: item.puntaje,
-      Riesgo: item.resultado_riesgo
-    }));
+    const data =
+      this.dataSource.data.map(item => ({
+        Fecha: item.fecha_evaluacion,
+        IMC: item.imc,
+        Puntaje: item.puntaje,
+        Riesgo: item.resultado_riesgo
+      }));
 
-    const worksheet = XLSX.utils.json_to_sheet(data);
-    const workbook = XLSX.utils.book_new();
+    const worksheet =
+      XLSX.utils.json_to_sheet(data);
 
-    XLSX.utils.book_append_sheet(workbook, worksheet, 'Historial');
-    XLSX.writeFile( workbook,'historial-evaluaciones.xlsx');
+    const workbook =
+      XLSX.utils.book_new();
+
+    XLSX.utils.book_append_sheet(
+      workbook,
+      worksheet,
+      'Historial'
+    );
+
+    XLSX.writeFile(
+      workbook,
+      'historial-evaluaciones.xlsx'
+    );
   }
 }
