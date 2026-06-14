567008a567a3df0ff88d29e0b6cde66485c2b24a
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-06-14 15:13:39 -0500

    feat: se agrego el nuevo componente en el modulo

--- src/app/module/pages/cuestionarios-autoevaluacion/cuestionarios-autoevaluacion-module.ts
+++ src/app/module/pages/cuestionarios-autoevaluacion/cuestionarios-autoevaluacion-module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 import { CuestionariosAutoevaluacionRoutingModule } from './cuestionarios-autoevaluacion-routing-module';
 import { Cuestionario } from './cuestionario/cuestionario';
 import { MaterialModule } from '../../../shared/ui/material-module';
+import { FormsModule } from '@angular/forms';
+import { Administracion } from './administracion/administracion';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 
 @NgModule({
   declarations: [
-    Cuestionario
+    Cuestionario,
+    Administracion
   ],
   imports: [
     MaterialModule,
+    FormsModule,
+    DragDropModule,
     CuestionariosAutoevaluacionRoutingModule
   ]
 })
