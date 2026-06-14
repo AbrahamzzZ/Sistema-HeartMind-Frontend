80ae407f956535a2cf5e166f418ef85e14ccbc64
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-06-14 15:14:16 -0500

    feat: se desarrollo la logica para cargar la informacion del cuestionario

--- src/app/module/pages/cuestionarios-autoevaluacion/administracion/administracion.ts
+++ src/app/module/pages/cuestionarios-autoevaluacion/administracion/administracion.ts
@@ -1,6 +1,8 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CuestionarioService } from '../../../../core/service/cuestionario.service';
+import Swal from 'sweetalert2';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-administracion',
@@ -8,21 +10,127 @@ import { CuestionarioService } from '../../../../core/service/cuestionario.servi
   templateUrl: './administracion.html',
   styleUrl: './administracion.scss',
 })
-export class Administracion implements OnInit{
+export class Administracion implements OnInit {
   private readonly route = inject(ActivatedRoute);
   private readonly servicio = inject(CuestionarioService);
+  private readonly router = inject(Router);
 
   id?: number;
   modoEdicion = false;
 
-  ngOnInit(): void {
+  cuestionario: any = {
+    titulo: '',
+    descripcion: '',
+    preguntas: []
+  };
 
+  nuevaPregunta = '';
+  opciones: string[] = ['', ''];
+
+  ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
 
     if (id) {
       this.id = Number(id);
       this.modoEdicion = true;
-      this.servicio.obtenerPorId(this.id);
+
+      this.servicio.obtenerPorId(this.id).subscribe({
+        next: (resp) => {
+          this.cuestionario = resp.data;
+        }
+      });
     }
+  }
+
+  agregarOpcion(): void {
+    if (this.opciones.length >= 6) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Máximo 6 opciones por pregunta.'
+      });
+      return;
+    }
+
+    this.opciones.push('');
+  }
+
+  eliminarOpcion(index: number): void {
+    this.opciones.splice(index, 1);
+  }
+
+  agregarPregunta(): void {
+
+    if (!this.nuevaPregunta.trim()) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'La pregunta no puede estar vacía.'
+      });
+      return;
+    }
+
+    const opcionesValidas = this.opciones
+      .map(o => o.trim())
+      .filter(o => o !== '');
+
+    if (opcionesValidas.length < 2) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Debe haber al menos 2 opciones.'
+      });
+      return;
+    }
+
+    const pregunta = {
+      pregunta: this.nuevaPregunta,
+      opciones: opcionesValidas.map(o => ({
+        texto: o,
+        esCorrecta: false
+      }))
+    };
+
+    this.cuestionario.preguntas.push(pregunta);
+    this.nuevaPregunta = '';
+    this.opciones = ['', ''];
+  }
+
+  eliminarPregunta(index: number): void {
+    this.cuestionario.preguntas.splice(index, 1);
+  }
+
+  guardar(): void {
+
+    this.servicio.crearCompleto(this.cuestionario).subscribe({
+      next: () => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Correcto',
+          text: 'Cuestionario creado correctamente'
+        });
+
+        this.router.navigate(['/home/cuestionario']);
+      },
+      error: (err) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: err.error?.message ?? 'No se pudo procesar el cuestionario.'
+        });
+      }
+    });
+  }
+
+  cancelar(): void {
+    history.back();
+  }
+
+  drop(event: CdkDragDrop<any[]>): void {
+    moveItemInArray(
+      this.cuestionario.preguntas,
+      event.previousIndex,
+      event.currentIndex
+    );
   }
 }
